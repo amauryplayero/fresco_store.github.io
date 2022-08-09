a58c8922acfab3f09911b448542ad655fdcf460f
Amaury <amaury.lopezdiaz@gmail.com>
2022-08-09 15:57:34 -0400

    added leave a message

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,37 @@ export default function Projects() {
     <section id="projects">
     <div id="projectsContainer">
         <h1 id="projectsTitle">Projects</h1>
+        <div class="projectBoxContainer">
+                <div class="infoContainer">
+                    <div class="infoContainerThreeWay">
+                        <div class="projectTitleContainer">
+                            <h3>Leave A Message! <a href='https://github.com/amauryplayero/Social' target="_blank" class="githubDemo"><i class="devicon-github-original-wordmark"></i></a></h3>
+                            
+                        </div>
+
+                        <div class="projectTextContainer">
+                            <p>A personal blog where anyone can leave a message in the form of text or an image. Leave yours now! Made with Test Driver Development, Typescript, React, NodeJS, AWS:S3, and PostgreSQL, and a mobile-first intent.
+                            </p>
+                        </div>
+
+                        <div class="projectButtonContainer">
+                            {/* <a class="demoButtons" href="https://www.youtube.com/watch?v=BKfhRIqHaEU" target="_blank">VIDEO DEMO</a> */}
+                            <a class="demoButtons" href="https://leave-a-message-sooty.vercel.app/" target="_blank">LIVE DEMO</a>
+                            {/* <a class="demoButtons">VIDEO DEMO</a> */}
+                            
+                        </div>
+                           
+                    </div>
+                </div>
+
+                <div class="screenShotContainer">
+                    <div class="screenShotContainerForBorder">
+                        <img id="leaveAMessageElement"  src="https://i.imgur.com/7y3Rs8a.png"></img>
+                    </div>
+                </div>
+
+            </div>
+        {/* //////// */}
             <div class="projectBoxContainer">
                 <div class="infoContainer">
                     <div class="infoContainerThreeWay">
